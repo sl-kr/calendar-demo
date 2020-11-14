eab2fc19bdfc4530c37b507312b36d8a182b8ec8
Sam Rechsteiner <srechsteiner@ignitexds.com>
2020-11-14 11:40:09 -0500

    init

---
+++ src/components/Calendar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-calendar :events="events"></v-calendar>
+  </v-container>
+</template>
+
+<script>
+let date = new Date("11-15-2020")
+
+export default {
+  data() {
+    return {
+      events : [
+        {
+          name: 'name',
+          start: date,
+          end: date,
+          color: 'blue',
+          timed: false
+        }
+      ]
+    }
+  }
+}
+</script>
